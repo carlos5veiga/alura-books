58924714d63f6306051951253bfe41bf14573f9f
carlos5veiga <carlos5veiga@gmail.com>
2024-12-23 09:35:38 -0300

    Logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './imagens/logo.svg';
 import './App.css';
+import Logo from './componentes/Logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='logo'>
-          <img src={logo} alt='Imagem da logo'/>
-          <p><strong>Alura</strong>Books</p>
-        </div>
+        <Logo />
       </header>
     </div>
   );
