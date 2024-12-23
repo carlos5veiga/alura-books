c5dd17f9d5d74b66cbb6e3b87f90717e7ce46657
carlos5veiga <carlos5veiga@gmail.com>
2024-12-23 11:39:53 -0300

    estilizando

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,7 +4,10 @@ import logo from '../../imagens/logo.svg';
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='Imagem da logo' />
+            <img
+                src={logo}
+                alt='Imagem da logo'
+                className='logo-img' />
             <p><strong>Alura</strong>Books</p>
         </div>
     )
